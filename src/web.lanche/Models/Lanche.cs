fc0d0f7620ed7c8da7ad39618a4a9b26a8ec703a
Genilson De Maceno <gmaceno@4bio.com.br>
2022-09-27 20:43:38 -0300

    create model lanche and categoria

---
+++ src/web.lanche/Models/Lanche.cs
@@ -0,0 +1,25 @@
+namespace web.lanche.Views.Home
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+
+        public string Nome { get; set; }
+
+        public string DescricaoCurta { get; set; }
+
+        public string DescricaoDetalhada { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public string ImagemUrl { get; set; }
+
+        public string ImagemThumbnailUrl { get; set; }
+
+        public bool IsLanchePreferido { get; set; }
+
+        public bool EmEstoque { get; set; }
+
+        public Categoria CategoriaId { get; set; }
+    }
+}
