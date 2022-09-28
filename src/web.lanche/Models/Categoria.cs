adb6b4a080c0a1446962ce2706c76fda9591216f
Genilson De Maceno <gmaceno@4bio.com.br>
2022-09-27 21:23:35 -0300

    connection definition

--- src/web.lanche/Models/Categoria.cs
+++ src/web.lanche/Models/Categoria.cs
@@ -5,7 +5,6 @@ namespace web.lanche.Views.Home
         public int CategoriaId { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
-
         public List<Lanche> Lanches { get; set; }
     }
 }
