f0c47bf60870d6b0a690fd1b7d2387c82ffc7f09
Genilson De Maceno <gmaceno@4bio.com.br>
2022-10-06 21:11:35 -0300

    added dada categorias and lanches

---
+++ src/web.lanche/Migrations/20221006235200_includeDataCategoria.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace web.lanche.Migrations
+{
+    public partial class includeDataCategoria : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Categorias(Nome, Descricao) " +
+            "values('Normal','Lanche feito com igredientes normais')");
+
+            migrationBuilder.Sql("Insert into Categorias(Nome, Descricao) " +
+            "values('Natural','Lanche feito com ingredientes integrais e naturais')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categorias");
+        }
+    }
+}
