799ce93345721072415da5cb73e839f6c3fb7fe6
Genilson De Maceno <gmaceno@4bio.com.br>
2022-10-21 23:58:36 -0300

    added controller carrinho de compra and view index

--- src/web.lanche/Controllers/CarrinhoCompraController.cs
+++ src/web.lanche/Controllers/CarrinhoCompraController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using web.lanche.Models;
 using web.lanche.Repositories.Interfaces;
+using web.lanche.ViewModels;
 
 namespace web.lanche.Controllers
 {
@@ -13,7 +14,11 @@ namespace web.lanche.Controllers
         private readonly ILancheRepository _lancheRepository;
         private readonly CarrinhoCompra _carrinhoCompra;
 
-        public CarrinhoCompraController(ILancheRepository lancheRepository, CarrinhoCompra carrinhoCompra)
+        public CarrinhoCompraController
+        (
+            ILancheRepository lancheRepository, 
+            CarrinhoCompra carrinhoCompra
+        )
         {
             _lancheRepository = lancheRepository;
             _carrinhoCompra = carrinhoCompra;
@@ -21,7 +26,38 @@ namespace web.lanche.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var itens = _carrinhoCompra.GetCarrinhoCompraItems();
+            _carrinhoCompra.carrinhoCompraItems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel{
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraVM);
+        }
+
+        public IActionResult AdicionarItemCarrinhoCompra(int lancheId){
+            var lancheSelecionado = _lancheRepository.Lanches
+                                    .FirstOrDefault(p => p.LancheId == lancheId);
+
+            if (lancheSelecionado != null)
+            {
+                _carrinhoCompra.AdicionarCarrinho(lancheSelecionado);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RemoverItemCarrinhoCompra(int lancheId){
+            var lancheSelecionado = _lancheRepository.Lanches
+                                                     .FirstOrDefault(p => p.LancheId == lancheId);
+
+            if (lancheSelecionado != null)
+            {
+                _carrinhoCompra.RemoverCarrinho(lancheSelecionado);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
