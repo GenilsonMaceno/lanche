557d5c72721d765cd27401cd182396443aa46aef
Genilson De Maceno <gmaceno@4bio.com.br>
2022-10-23 23:26:14 -0300

    added components

--- src/web.lanche/Controllers/HomeController.cs
+++ src/web.lanche/Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using web.lanche.Models;
+using web.lanche.Repositories.Interfaces;
+using web.lanche.ViewModels;
 
 namespace web.lanche.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILancheRepository _lancheRepository;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILancheRepository lancheRepository)
     {
-        _logger = logger;
+        _lancheRepository = lancheRepository;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var homeViewModel = new HomeViewModel{
+            LanchesPreferidos = _lancheRepository.LanchesPreferidos
+        };
+
+        return View(homeViewModel);
     }
 
     public IActionResult Privacy()
---
+++ src/web.lanche/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+using web.lanche.Models;
+
+namespace web.lanche.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Lanche> LanchesPreferidos { get; set; }
+    }
+}
